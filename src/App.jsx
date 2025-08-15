540f702d1765658323ef31016b4d88671f926e36
rakshasubramanya08 <rakshasubramanya08@gmail.com>
2025-08-15 20:32:05 +0200

    Mounted Portal file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Portal from './FrontEnd/portal';
 
 function App() {
   return (
     <div>
-      
+      <Portal />
     </div>
   )
 }
 
 export default App; 
+''
