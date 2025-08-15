c206f893f66e5869d44b33adee75d3d0f79dfcc9
rakshasubramanya08 <rakshasubramanya08@gmail.com>
2025-08-16 01:19:37 +0200

    Added new file and info.

---
+++ src/FrontEnd/PersonalDetails.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { useEffect, useState} from 'react';
+import './PersonalDetails.css';
+
+const PersonalDetails = () => {
+
+    const [country, setCountry] = useState([]);         // [{code, name}]
+    const [nationality, setNationality] = useState(""); // selected value
+
+      useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all?fields=name,cca2")
+      .then((r) => r.json())
+      .then((data) => {
+        const list = data
+          .map((c) => ({ code: c.cca2, name: c.name.common }))
+          .sort((a, b) => a.name.localeCompare(b.name));
+        setCountry(list);
+      })
+      .catch(console.error);
+  }, []);
+
+  return (
+    <div>
+           <div className="body2">
+          <p><b><u>Personal details:</u></b></p>
+
+          <label htmlFor="name">Name:</label>
+          <input id="name" type="text" />
+
+          <label htmlFor="vorname">Vorname:</label>
+          <input id="vorname" type="text" />
+
+          <div className="body2">
+            <label htmlFor="dob">Date Of Birth:</label>
+            <input id="dob" type="date" />
+
+            <label htmlFor="nationality">Nationality:</label>
+            <select
+              id="nationality"
+              value={nationality}
+              onChange={(e) => setNationality(e.target.value)}
+            >
+              <option value="">Select a country</option>
+              {country.map((c) => (
+                <option key={c.code} value={c.code}>
+                  {c.name}
+                </option>
+              ))}
+            </select>
+            
+            <div className="body2">
+            <label htmlFor="phone">Phone:</label>
+            <input id="phone" type="text" />
+
+            <label htmlFor="email">Email:</label>
+            <input type="text" />
+            </div>  
+           
+          </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default PersonalDetails
