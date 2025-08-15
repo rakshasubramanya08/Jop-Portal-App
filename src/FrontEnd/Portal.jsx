f4c31e9a0bb74ccfbde434f27a10df3f76486202
rakshasubramanya08 <rakshasubramanya08@gmail.com>
2025-08-15 22:43:44 +0200

    Added all input tags and api fetched for selecting country.

--- src/FrontEnd/Portal.jsx
+++ src/FrontEnd/Portal.jsx
@@ -1,47 +1,81 @@
-import React from 'react'
-import './Portal.css';
-
+import React, { useEffect, useState } from "react";
+import "./Portal.css";
 
 function Portal() {
+  const [country, setCountry] = useState([]);         // [{code, name}]
+  const [nationality, setNationality] = useState(""); // selected value
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all?fields=name,cca2")
+      .then((r) => r.json())
+      .then((data) => {
+        const list = data
+          .map((c) => ({ code: c.cca2, name: c.name.common }))
+          .sort((a, b) => a.name.localeCompare(b.name));
+        setCountry(list);
+      })
+      .catch(console.error);
+  }, []);
+
   return (
     <>
-     <h1>Job Application Portal</h1> 
+      <h1 className="heading">Job Application Portal</h1>
 
-     <div className="body1">
-        <label htmlFor='jobTitle'>Job Title</label>
-        <input type="text" />
-     </div>
+      <div className="body1">
+        <p><b><u>Job Title:</u></b></p>
+        <h2>FrontEnd Developer</h2>
+      </div>
 
-     <div className="body2">
-        <label htmlFor="name">Name:</label>  
-        <input type="text" />
+      <div className="box">
+        <div className="body2">
+          <p><b><u>Personal details:</u></b></p>
 
-        <label htmlFor='Vorname:'>Vorname:</label>
-        <input type="text" />
+          <label htmlFor="name">Name:</label>
+          <input id="name" type="text" />
 
-        <label htmlFor="DOB">Date Of Birth:</label>
-        <input type="datetime-local" />
+          <label htmlFor="vorname">Vorname:</label>
+          <input id="vorname" type="text" />
 
-        <label htmlFor="Nationality">Nationality:</label>
-        <input type="text" />
-     </div>
+          <div className="body2">
+            <label htmlFor="dob">Date Of Birth:</label>
+            <input id="dob" type="date" />
 
-     <div className="body3">
-        <p>Address</p>
-        <label htmlFor="street">Street:</label>
-        <input type="text" />
+            <label htmlFor="nationality">Nationality:</label>
+            <select
+              id="nationality"
+              value={nationality}
+              onChange={(e) => setNationality(e.target.value)}
+            >
+              <option value="">Select a country</option>
+              {country.map((c) => (
+                <option key={c.code} value={c.code}>
+                  {c.name}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
 
-        <label htmlFor="city">City:</label>
-        <input type="text" />
+        <div className="body3">
+          <p><b><u>Address:</u></b></p>
 
-        <label htmlFor="Zip">Zip:</label>
-        <input type="text" />
+          <label htmlFor="street">Street:</label>
+          <input id="street" type="text" />
 
-        <label htmlFor="Phone">Phone</label>
-        <input type="text" />
-     </div>
+          <label htmlFor="city">City:</label>
+          <input id="city" type="text" />
+
+          <div className="body3">
+            <label htmlFor="zip">Zip:</label>
+            <input id="zip" type="text" />
+
+            <label htmlFor="phone">Phone:</label>
+            <input id="phone" type="text" />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Portal
+export default Portal;
